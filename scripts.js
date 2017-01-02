5e2fc10ebc71890e92c158e8166904897220a6a3
Chā <chavis.clark@gmail.com>
2017-01-01 19:58:44 -0500

    added sub headings

--- scripts.js
+++ scripts.js
@@ -1,35 +1,46 @@
 function updateOutput() {
   $("iframe").contents().find("html").html(
     "<html><head><style type='text/css'>" + 
-    $("#cssPanel").val() + "</style></head><body>" + 
-    $("#htmlPanel").val() + "</body></html>"
+    $("#cssBody").val() + "</style></head><body>" + 
+    $("#htmlBody").val() + "</body></html>"
     );
-    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
 }
-
+///Toggle highlight function
 $(".toggleButton").hover(function() {
   $(this).addClass("highlightedButton")
 }, function() {
   $(this).removeClass("highlightedButton")
 });
 
+///Toggle active panel function
 $(".toggleButton").click(function() {
-  $(this).toggleClass("active");
-  $(this).removeClass("highlightedButton");
-
   var panelId = $(this).attr("id") + "Panel";
 
+  $(this).toggleClass("active");
+  $(this).removeClass("highlightedButton");
   $("#" + panelId).toggleClass("hidden");
 
-  var numberOfActivePanels = 4 - $('.hidden').length;
-  $(".panel").width(($(window).width() / numberOfActivePanels ) - 10);
+  numberOfActivePanels = 4 - $('.hidden').length;
+  $(".panelBody").width(($(window).width() / numberOfActivePanels ) - 10);
 });
 
-$(".panel").height($(window).height() - $("#header").height() - 15);
+var numberOfActivePanels = 4 - $('.hidden').length;
+$(".panelBody").width(($(window).width() / numberOfActivePanels ) - 10);
+
+/// Set height of bodyContainer and margins
+var headerHeight = $("#header").height() * 1.4;
+$("#bodyContainer").css('top', headerHeight + "px");
+$(".panel > span").css('margin-top', headerHeight + "px");
+$("#bodyContainer").height($(window).height() - (headerHeight * 3.2) );
 
 updateOutput();
 
-$("textarea").on('change keyup paste', function() {
 
+///Handle events on textarea elements
+$("textarea").on('change keyup paste', function() {
   updateOutput();
-})
+});
+
+$("textarea").on('blur', function() {
+  document.getElementById("outputBody").contentWindow.eval($("#javascriptBody").val());
+});
